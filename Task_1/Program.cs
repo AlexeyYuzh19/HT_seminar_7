523ac9e1f429c6fb8b712868103e0c35bc0a53f0
AlexeyYuzh19 <9676331836@mail.ru>
2022-12-21 15:57:16 +0500

    код для задачи 47 с методами

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -9,103 +9,7 @@ m = 3, n = 4.
 Console.Clear();
 Console.WriteLine("Hello, World!");
 
-NAMBERS(out int size_0, out int size_1, out int leftRange, out int rightRange);
-
-FillAndPrintArray(size_0, size_1, leftRange, rightRange);
-
 // Методы
-void FillAndPrintArray(int m, int n, int leftRange, int rightRange)
-{
-    double[,] array = new double[m, n];
-
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = Math.Round((new Random().Next(leftRange, rightRange) + (Math.Abs(new Random().NextDouble()))), 3);
-            Console.ForegroundColor = ConsoleColor.Green;
-            var ar = String.Format("{0,10}", array[i, j]);
-            System.Console.Write(ar);
-            Console.ResetColor();
-            System.Console.Write(" | ");
-        }
-        System.Console.WriteLine();
-    }
-}
-
-/*
-double[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
-{
-    double[] tempArr = new double[size];
-    Random rand = new Random();
-    for(int i = 0; i < tempArr.Length; i++)
-    {
-        tempArr[i] = rand.Next(leftRange, rightRange) - Math.Round(rand.NextDouble(), 3);
-    }
-    return tempArr;
-}
-*/
-
-int CheckSize(string text)
-{
-    int size;
-    while (true)
-    {
-        size = CheckInputNumber(text);
-
-        if (size < 0) Console.WriteLine("Задано отрицательное значение, попробуйте еще раз.");
-
-        else if (size == 0) Console.WriteLine("Задано нулевое значение, попробуйте еще раз.");
-
-        else break;
-    }
-    return size;
-}
-
-void NAMBERS(out int size_0, out int size_1, out int leftRange, out int rightRange)
-{
-   size_0 = CheckSize("Введите количество строк массива : ");
-
-   size_1 = CheckSize("Введите количество столбцов массива : ");
-   
-   /*
-    while (true)
-    {
-        size_0 = CheckInputNumber("Введите количество строк массива от двух элементов : ");
-
-        if (size_0 < 0) Console.WriteLine("Задано отрицательное значение, попробуйте еще раз.");
-
-        else if (size_0 == 0) Console.WriteLine("Задано нулевое значение, попробуйте еще раз.");
-
-        else if (size_0 == 1) Console.WriteLine("Задан массив с одним элементом, попробуйте еще раз.");
-
-        else break;
-    }
-
-    while (true)
-    {
-        size_1 = CheckInputNumber("Введите размер столбцов массива от двух элементов : ");
-
-        if (size_1 < 0) Console.WriteLine("Задано отрицательное значение, попробуйте еще раз.");
-
-        else if (size_1 == 0) Console.WriteLine("Задано нулевое значение, попробуйте еще раз.");
-
-        else if (size_1 == 1) Console.WriteLine("Задан массив с одним элементом, попробуйте еще раз.");
-
-        else break;
-    }
-*/
-    while (true)
-    {
-        leftRange = CheckInputNumber("Введите величину левого значения массива : ");
-
-        rightRange = CheckInputNumber("Введите величину правого значения массива : ");
-
-        if (leftRange <= rightRange) break;
-
-        Console.WriteLine("Заданное значение левого края массива больше правого, попробуйте еще раз.");
-    }
-}
 
 int CheckInputNumber(string Text)
 {
@@ -128,3 +32,70 @@ int CheckInputNumber(string Text)
     Console.ResetColor();
     return number;
 }
+
+int CheckSize(string text)
+{
+    int size;
+    while (true)
+    {
+        size = CheckInputNumber(text);
+
+        if (size < 0) Console.WriteLine("Задано отрицательное значение, попробуйте еще раз.");
+
+        else if (size == 0) Console.WriteLine("Задано нулевое значение, попробуйте еще раз.");
+
+        else break;
+    }
+    return size;
+}
+
+void NAMBERS(out int size_0, out int size_1, out int leftRange, out int rightRange)
+{
+    size_0 = CheckSize("Введите количество строк массива : ");
+
+    size_1 = CheckSize("Введите количество столбцов массива : ");
+
+
+    while (true)
+    {
+        leftRange = CheckInputNumber("Введите величину левого значения (края) массива : ");
+
+        rightRange = CheckInputNumber("Введите величину правого значения (края) массива : ");
+
+        if (leftRange < rightRange) break;
+
+        else if (leftRange == rightRange) Console.WriteLine("Значения левого и правого края массива равны, попробуйте еще раз.");
+
+        else Console.WriteLine("Заданное значение левого края массива больше правого, попробуйте еще раз.");
+    }
+}
+
+void MakePrintArray(int m, int n, int leftRange, int rightRange)
+{
+    double[,] array = new double[m, n];
+    Random rand = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round((rand.Next(leftRange, rightRange) + rand.NextDouble()), 3);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            var ar = String.Format("{0,10}", array[i, j]);
+
+            System.Console.Write(ar);
+            Console.ResetColor();
+
+            if (j < array.GetLength(1) - 1) System.Console.Write(" | ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+//  Код задачи
+
+NAMBERS(out int size_0, out int size_1, out int leftRange, out int rightRange);
+
+MakePrintArray(size_0, size_1, leftRange, rightRange);
